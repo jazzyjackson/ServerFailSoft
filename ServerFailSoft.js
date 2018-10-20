0a3b19668bf189888c803b76d71e8b4d9e9bc39f
Colten Jackson <jazzyjackson@gmail.com>
2018-10-19 21:32:55 -0500

    Prepping for 2.0: no longer using source.statusCode/headers, upgrade to using json map for error codes.

--- ServerFailSoft.js
+++ ServerFailSoft.js
@@ -1,60 +1,44 @@
 /**
  * @author Colten Jackson
- * ServerFailSoft attaches an error handler to a source stream piped to it.
- * It will pass on the .statusCode and .headers of the source stream once
- * EITHER the first byte is received, or the the end event is received.
- * Source streams should be designed to error sooner rather than later.
- * If an error is received before any bytes are sent, a proper statuscode
- * can be transmitted and informative error object can be sent in whole.
- *
+ * Using ServerFailSoft as your http.ServerResponse allows informative errors
+ * to be sent to the client when an error is emitted by a stream being piped.
+ * 
  * If, however, something goes wrong halfway through writing a response,
  * I will still concatenate the JSON error string to whatever bytes were sent.
- * This is not ideal, but probably better than swallowing the error silently.
+ * This is not ideal, but I think its better than swallowing the error silently.
  */
 const { ServerResponse } = require('http')
+const os = require('os')
+const errorMap = require('./errorMap.json')
 
 module.exports = class ServerFailSoft extends ServerResponse {
     constructor(res){
         super(res)
         this.once('pipe', source => {
-            /* Getters with defaults for statusCode and headers exist on Transflect, but just in case: */
-            if(!source.statusCode) source.statusCode = 200
-            if(!source.headers) source.headers = {}
-            /* writeHead would be called on first byte, prepend the listener to get to it first! */
-            source.prependOnceListener('data', () => {
-                this.headersSent || this.writeHead(source.statusCode, source.headers)
-            })
-            /* make sure status and headers are also transferred if no data is sent */
-            source.prependOnceListener('end', () => {
-                this.headersSent || this.writeHead(source.statusCode, source.headers)
-            })
-            /* getuid, getgid, getgroups are gaurded and fallback to undefined on Windows */
             source.once('error', error => {
                 /* prepare error response: prettyprint with 2 space indent */
                 /* NOTE: errObj may be empty but still carry a message when stringified */
+                let status = errorMap[ error.code ] || 500
                 let errorResponse = JSON.stringify({
                     source:   source.constructor.name,
-                    errMsg:   error.toString(),
+                    status:   status,
                     errObj:   error,
+                    errMesg:  error.toString(),
+                    errStack: error.stack.split(os.EOL),
                     method:   this.connection.parser.incoming.method,
                     url:      this.connection.parser.incoming.url,
                     versions: process.versions,
                     platform: process.platform,
                     uid:      process.getuid    && process.getuid(),
                     gid:      process.getgid    && process.getgid(),
-                    groups:   process.getgroups && process.getgroups()
+                    groups:   process.getgroups && process.getgroups(),
                 }, null, 2) 
                 /* this may be extended for custom error codes, but try to stick to standards! */
-                this.headersSent || this.writeHead(
-                    error.code == 'ENOENT'  ? 404 : // error no entity
-                    error.code == 'ENOTDIR' ? 404 : // file is not a directory
-                    error.code == 'ERANGE'  ? 416 : // range not satisfiable
-                    error.code == 'EACCES'  ? 423 : // error no access (permission)
-                    /* no code ? default: */  500 , /* + headers: */ {
+                this.headersSent || this.writeHead(status, {
                     'Content-Length': Buffer.byteLength(errorResponse),
                     'Content-Type': 'application/json',
                 })
-                this.end(errorResponse)
+                this.writable && this.end(errorResponse)
             })
         })
     }
--- readme.md
+++ readme.md
@@ -5,10 +5,11 @@ If you open a stream and pipe it to ServerFailSoft, those bytes are streamed to 
 
 The first bytes piped to ServerFailSoft will trigger the `.writeHead` function, which will attempt to read `statusCode` and `headers` properties from the source. It also attempts to writeHead from source on the `end` event, in case there wasn't any data.
 
-If the source throws an error (before bytes are sent!), ServerFailSoft will catch it and prepare an informative error response. (Perhaps TOO informative for your tastes if you wish to keep your software versions to yourself, but I prefer to advertise that my server is up-to-date.)
+If the source throws an error (before bytes are sent!), ServerFailSoft will catch it and prepare an informative error response. (Perhaps TOO informative for your tastes if you wish to keep your software versions and stack traces to yourself) - I hope it's clear how to modify the errorresponse object (you can just comment out lines you don't want) to fork this repo and have it suit your tastes.
 
 ### Usage
 Since Node v9.6.0, http.createServer supports an options argument to override the IncomingMessage and ServerResponse constructed for each request.
+
 
 ```js
 http.createServer({
@@ -19,11 +20,9 @@ http.createServer({
     fs.createReadStream(filepath).pipe(res)
 }).listen(3000)
 ```
-You can run `node examples/staticserver.js` to try this out.
+You can run `node examples/staticserver.js` to try this out. If you request a file that doesn't exist, you'll see the ENOENT error code and everything else.
 
-See more details / ideas in the [Transflect](/mixint/Transflect) repository - Transflect is an extension of stream.Transform that allows you to safely open requisite streams to respond to requests while avoiding memory and file descriptor leaks.
-
-An example server where PassThrough (like Transform but without the `_transform`) is used to create a pipeline:
+Besides using built in streams, consider writing your own streams and piping them to ServerFailSoft. Here's an example server where PassThrough (like Transform but without the `_transform`) is used to create a pipeline that fails half the time, and compliments your good luck the other half.
 
 ```js
 // an example class that fails half the time
@@ -46,3 +45,16 @@ http.createServer({
 }).listen(3000)
 ```
 You can run `node examples/luckyserver.js` to try it out.
+
+### Extending
+
+Pull requests accepted, I've started an errorMap.json to assign proper statusCodes (404 for ENOENT, no entity exists, 423 for EACCESS, insufficient permissions, 416 for the error code thrown by handing nonsensical start and end numbers to fs.createReadStream ).
+
+```js
+{
+    "ENOENT": 404,
+    "ENOTDIR": 404,
+    "EACCESS": 423,
+    "ERR_OUT_OF_RANGE": 416,
+}
+```
